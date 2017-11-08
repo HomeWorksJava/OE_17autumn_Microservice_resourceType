06afbfa4d917a206feac98fa14209d29ecc96dc5
Hazai Péter <>
2017-11-08 13:53:15 +0100

    Created SpringBootService model project.

---
+++ Microservice_ResourceTypes/ResourceTypes_SpringBootService/src/main/java/hu/microservices/homework/resourcetype/ResourceTypes_SpringBootService/ResourceTypesSpringBootServiceApplication.java
@@ -0,0 +1,12 @@
+package hu.microservices.homework.resourcetype.ResourceTypes_SpringBootService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceTypesSpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceTypesSpringBootServiceApplication.class, args);
+	}
+}
---
+++ Microservice_ResourceTypes/ResourceTypes_SpringBootService/src/test/java/hu/microservices/homework/resourcetype/ResourceTypes_SpringBootService/ResourceTypesSpringBootServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.microservices.homework.resourcetype.ResourceTypes_SpringBootService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourceTypesSpringBootServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
