d06a1c0bc0c73e1884e18418eb556e0f58c2f36d
Hazai Péter <hazaip@niif.hu>
2017-11-22 12:00:21 +0100

    cleanup, create new spring boot project, include classes

---
+++ Microservice_ResourceTypes/ResourceTypes_SpringBootService/src/main/java/hu/microservices/homework/resourcetype/springbootservice/ResourceTypesSpringBootServiceApplication.java
@@ -0,0 +1,12 @@
+package hu.microservices.homework.resourcetype.springbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceTypesSpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceTypesSpringBootServiceApplication.class, args);
+	}
+}
